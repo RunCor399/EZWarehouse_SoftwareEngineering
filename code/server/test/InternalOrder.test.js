a543c3e7152f7fef2e72c4a111e938fcf746786c
GIOVANNA DI_BENEDETTO s258792 <s258792@studenti.polito.it>
2022-05-19 10:06:36 +0200

    first draft of Internal Order tests

--- code/server/test/InternalOrder.test.js
+++ code/server/test/InternalOrder.test.js
@@ -15,5 +15,41 @@ const utilityCalls = new UtilityCalls();
 const internalOrdersAPICalls = new InternalOrdersAPICalls();
 
 describe('internal order test suite', async () => {
-    /*insert test cases*/
+    describe('Standard Internal Order getters', async () => {
+        it('get all internal orders', async () => {
+            const response = await internalOrdersAPICalls.getInternalOrdersTest();
+
+            response.status.should.equal(200);
+        });
+
+        it('get issued internal orders', async () => {
+            const response = await internalOrdersAPICalls.getIssuedInternalOrdersTest();
+
+            response.status.should.equal(200);
+        });
+
+        it('get accepted internal orders', async () => {
+            const response = await internalOrdersAPICalls.getAcceptedInternalOrdersTest();
+
+            response.status.should.equal(200);
+        });
+
+        it('get internal order by id', async () => {
+            const response = await internalOrdersAPICalls.getInternalOrderByIdTest(1);
+
+            response.status.should.equal(200);
+        });
+    });
 });
+
+describe('POST Request test to Internal Orders', async () => {
+
+});
+
+describe('PUT Request test to Internal Orders', async() => {
+
+});
+
+describe('DELETE Request test to Internal Orders', async() => {
+    
+})
