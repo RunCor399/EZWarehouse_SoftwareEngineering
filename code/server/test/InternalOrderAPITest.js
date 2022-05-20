a4b593f53bd23e0254c6505a797fcc2a046aeb60
GIOVANNA DI_BENEDETTO s258792 <s258792@studenti.polito.it>
2022-05-20 17:52:19 +0200

    modified InternalOrderAPITest.js

--- code/server/test/InternalOrderAPITest.js
+++ code/server/test/InternalOrderAPITest.js
@@ -8,14 +8,59 @@ const axios = require('axios');
 
 const UtilityCalls = require('./APICalls/UtilityCalls');
 const InternalOrdersAPICalls = require('./APICalls/InternalOrdersAPICalls');
+const DBManager = require('../modules/database/databaseManager');
 
 const baseURL = "http://localhost:3001";
 
-const utilityCalls = new UtilityCalls();
+//const utilityCalls = new UtilityCalls();
+const databaseManager = new DBManager();
 const internalOrdersAPICalls = new InternalOrdersAPICalls();
 
 describe('Internal Orders test suite', async () => {
-    describe('Standard Internal Order getters', async () => {
+
+    /*beforeEach(async () => {
+        await databaseManager.deleteAllData().then(async () => {
+            //insert internal order data
+        });
+      });
+
+    after(async () => {
+        await databaseManager.deleteAllData();
+    })*/
+
+    before(async () => {
+        await dbmanager.deleteAllData();
+    })
+    after(async () => {
+        await dbmanager.deleteAllData();
+    })
+
+    describe('POST Request test to Internal Orders', async () => {
+        describe('Add a new Internal Order tests', async () => {
+            let response;
+            it('Successfully add a new Internal Order', async () => {
+                response = await internalOrdersAPICalls.addInternalOrderTest("2022/02/02 10:10", [], 2);
+                response.status.should.equal(201);
+
+                /*No data has been added before this add, therefore the id is 1*/
+                response = await internalOrdersAPICalls.getInternalOrderByIdTest(1);
+                response.data.id.should.equal(1);
+            });
+
+            it('Negative customerId', async () => {
+                response = await internalOrdersAPICalls.addInternalOrderTest("2022/02/04 13:00", [], -5);
+                response.status.should.equal(422);
+            });
+
+            it('Malformed date', async () => {
+                response = await internalOrdersAPICalls.addInternalOrderTest("05/02/2022 10:10", [], 10);
+                response.status.should.equal(422);
+            });
+
+        });
+    });
+
+    describe('GET Request tests to Internal Orders', async () => {
         it('get all internal orders', async () => {
             const response = await internalOrdersAPICalls.getInternalOrdersTest();
 
@@ -40,34 +85,25 @@ describe('Internal Orders test suite', async () => {
             response.status.should.equal(200);
         });
     });
-});
 
-describe('POST Request test to Internal Orders', async () => {
-    describe('Add a new Internal Order tests', async () => {
-        it('Successfully add a new Internal Order', async () => {
+    describe('PUT Request test to Internal Orders', async () => {
+        describe('Edit an existing Internal Order tests', async () => {
+            it('Successfully edit an Internal Order', async () => {
 
+            });
+
+            /*add error test cases*/
         });
-
-        /*add error test cases*/
     });
-});
 
-describe('PUT Request test to Internal Orders', async () => {
-    describe('Edit an existing Internal Order tests', async () => {
-        it('Successfully edit an Internal Order', async () => {
+    describe('DELETE Request test to Internal Orders', async () => {
+        describe('Delete an Internal Order tests', async () => {
+            it('Successfully delete an Internal Order', async () => {
 
+            });
+
+            /*add error test cases*/
         });
+    })
 
-        /*add error test cases*/
-    });
 });
-
-describe('DELETE Request test to Internal Orders', async () => {
-    describe('Delete an Internal Order tests', async () => {
-        it('Successfully delete an Internal Order', async () => {
-
-        });
-
-        /*add error test cases*/
-    });
-})
