4781c82f81ebe1fe2136fbe666d5cecda3899774
GIOVANNA DI_BENEDETTO s258792 <s258792@studenti.polito.it>
2022-05-25 11:01:25 +0200

    added TestDescriptor.test.js

---
+++ code/server/unit_test/TestDescriptor.test.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const { expect } = require('chai');
+const Controller = require('../modules/logic/controller');
+
+const controller = new Controller();
+const testDescriptorController = controller.getTestDescriptorController();
+const dbManager = controller.getDBManager();
+
+beforeEach(async () => {
+    await dbManager.deleteAllData().then(async () => {
+        //await dbManager.insertTestDescriptorTestData();
+    })
+});
+
+afterEach(async () => {
+    await dbManager.deleteAllData();
+});
+
+describe("TestDescriptorController Tests", () => {
+    describe("getTestDescriptor method testing", () => {
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+    });
+    
+    describe("createTestDescriptor method testing", () => {
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+    });
+    
+    describe("editTestDescriptor method testing", () => {
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+    });
+    
+    describe("deleteTestDescriptor method testing", () => {
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+        
+        test("", () => {
+
+        });
+    });
+});
