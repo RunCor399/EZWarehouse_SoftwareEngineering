16cf402dc9fcfb1ba2dd72d357cce1bad7dee418
GIOVANNA DI_BENEDETTO s258792 <s258792@studenti.polito.it>
2022-05-25 19:11:04 +0200

    modified TestDescriptor.test.js

--- code/server/unit_test/TestDescriptor.test.js
+++ code/server/unit_test/TestDescriptor.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const Controller = require('../modules/logic/controller');
 
 const controller = new Controller();
@@ -59,31 +59,89 @@ describe("TestDescriptorController Tests", () => {
 
             await testDescriptorController.createTestDescriptor(body);
 
-            result = await testDescriptorController.getTestDescriptor(currId).catch(() => {});
+            result = await testDescriptorController.getTestDescriptor(currId).catch(() => { });
 
             expect(result).not.to.be.undefined;
         });
 
-        test("Insertion of a test descriptor with a non-existing SKU", () => {
+        test("Insertion of a test descriptor with a non-existing SKU", async () => {
+            let result;
+            let currId;
+            const body = {
+                name: "new_test_descriptor",
+                procedureDescription: "procedureDescription13",
+                idSKU: 13000
+            };
+            currId = ((await testDescriptorController.getAllTestDescriptors()).length) + 1;
 
+            await testDescriptorController.createTestDescriptor(body).catch(() => { });
+
+            result = await testDescriptorController.getTestDescriptor(currId).catch(() => { });
+
+            expect(result).to.be.undefined;
         });
 
-        test("Insertion of a test descriptor with invalid body", () => {
+        test("Insertion of a test descriptor with invalid body", async () => {
+            let result;
+            let currId;
+            const body = {
+                name: "another_test_descriptor",
+                procedureDescription: "procedureDescription44",
+                idSKU: "anSKU"
+            };
+            currId = ((await testDescriptorController.getAllTestDescriptors()).length) + 1;
 
+            await testDescriptorController.createTestDescriptor(body).catch(() => { });
+
+            result = await testDescriptorController.getTestDescriptor(currId).catch(() => { });
+
+            expect(result).to.be.undefined;
         });
     });
 
     describe("editTestDescriptor method testing", () => {
-        test("Successfully edit a Test Descriptor", () => {
+        test("Successfully edit a Test Descriptor", async () => {
+            let result;
+            let currId;
+            const body = {
+                newName: "newName",
+                newProcedureDescription: "newProcedureDescription",
+                newIdSKU: 2
+            };
+            let thisId;
+            
+            currId = ((await testDescriptorController.getAllTestDescriptors()).length);
 
+            await testDescriptorController.editTestDescriptor(currId, body);
+            result = testDescriptorController.getTestDescriptor(currId);
+            thisId = result['id'];
+
+            expect(currId).to.be.equal(thisId);
         });
 
-        test("Edit a Test Descriptor with invalid id", () => {
+        test("Edit a Test Descriptor with invalid id", async () => {
+            let result;
+            const body = {
+                newName: "newName",
+                newProcedureDescription: "newProcedureDescription",
+                newIdSKU: 2
+            };
 
+            result = await testDescriptorController.editTestDescriptor(-1, body).catch(() => { });
+            expect(result).to.be.undefined;
         });
 
-        test("Edit a Test Descriptor with a non-exitent SKU", () => {
+        test("Edit a Test Descriptor with a non-existent SKU", async () => {
+            let result;
+            const body = {
+                newName: "newName",
+                newProcedureDescription: "newProcedureDescription",
+                newIdSKU: 20000
+            };
 
+            result = await testDescriptorController.editTestDescriptor(1, body).catch(() => { });
+
+            expect(result).to.be.undefined;
         });
     });
 
